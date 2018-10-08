e7c7708937a167af563c29a0a5f50253c44136a5
zemenuyar <zemenuyar@gmail.com>
2018-10-08 14:12:56 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //difine the limit
+        int num =20;
+        System.out.println("print even number beteween 1" + num);
+        for (int i=0; i<20; i++){
+            // if the number is divisible by 2 then it is even.
+            if (i%2==0){
+                System.out.println(i);
+            }
+        }
+
+
+    }
+}
